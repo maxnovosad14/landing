cadc8646db46ad0ea71466ce50ed1f7777f72a09
max <maxnovosad12@gamil.com>
2022-08-03 12:19:31 +0300

    section providing

---
+++ src/app/components/providing/providing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-providing',
+  templateUrl: './providing.component.html',
+  styleUrls: ['./providing.component.scss']
+})
+export class ProvidingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
